f48d188d91706712c07d8e8e384fa3dc8018df07
Kairooe <Kevin.kbt.1@gmail.com>
2024-11-08 10:25:59 -0800

    cardDetails page working

--- assignment5.js
+++ assignment5.js
@@ -27,6 +27,31 @@ app.get('/api/cards', (req, res) => {
     });
 });
 
+// Add this new route for individual card details
+app.get('/api/card/:id', (req, res) => {
+    const filePath = path.join(__dirname, 'app', 'data', 'pokemon.json');
+    const id = parseInt(req.params.id);
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading pokemon.json:', err);
+            res.status(500).send('Error reading card data');
+            return;
+        }
+        try {
+            const cards = JSON.parse(data);
+            if (id >= 0 && id < cards.length) {
+                res.json(cards[id]);
+            } else {
+                res.status(404).send('Card not found');
+            }
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+            res.status(500).send('Invalid JSON format');
+        }
+    });
+});
+
 // Route for Magic cards HTML
 app.get('/api/featured-cards', (req, res) => {
     const filePath = path.join(__dirname, 'app', 'html', 'magic.html');
